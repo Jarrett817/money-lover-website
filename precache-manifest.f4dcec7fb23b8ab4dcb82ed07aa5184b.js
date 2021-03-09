bf7e34d27d1c3be73948a5ff294787f3a395c476
Jarrett <1206905682@qq.com>
2021-03-09 23:48:31 +0800

    deploy

---
+++ precache-manifest.f4dcec7fb23b8ab4dcb82ed07aa5184b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff625e9f52e6cff8105",
+    "url": "/money-lover-website/css/app.91c4311b.css"
+  },
+  {
+    "revision": "9d938f496fdcc01469b8853233f6d9fa",
+    "url": "/money-lover-website/index.html"
+  },
+  {
+    "revision": "4ff625e9f52e6cff8105",
+    "url": "/money-lover-website/js/app.5c9d8ae2.js"
+  },
+  {
+    "revision": "bfb0b5ef1a1da18ad944",
+    "url": "/money-lover-website/js/chunk-vendors.9c6f89a2.js"
+  },
+  {
+    "revision": "bcfe2a8a26ab6d275f270fa0530608ca",
+    "url": "/money-lover-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-lover-website/robots.txt"
+  }
+]);
